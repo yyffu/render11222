1af9160409c370ffeaf30b0f2b5188552bb8a31f
yyffu <100259439+yyffu@users.noreply.github.com>
2023-11-22 17:29:54 +1200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="a71beca9-c46b-48f8-be8f-eb0975649470"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
